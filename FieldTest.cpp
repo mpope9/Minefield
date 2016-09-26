bbee87fb9616a6d0880a2e80a3c95af01e7d429a
mpope9 <mpope9@mail.csuchico.edu>
2016-09-26 11:51:14 -0700

    Added several tests

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,57 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+/* Test for inboundsness */
+TEST(FieldTest, placeMineOutBounds)
+{
+   Field minefield(3);
+
+   minefield.placeMine(-1, 4);
+   ASSERT_ANY_THROW(minefield.get(-1, 4));
+}
+
+/* Tests if an exception is thrown for isSafe */
+TEST(FieldTest, isSafeThrow)
+{
+   Field minefield(3);
+
+   minefield.placeMine(-1, 4);
+   ASSERT_ANY_THROW(minefield.isSafe(-1, 4));
+}
+
+/* Tests if isSafe shows a safe 'square' */
+TEST(FieldTest, isSafeSafetyTest)
+{
+   Field minefield(3);
+
+   ASSERT_TRUE(minefield.isSafe(1, 1));
+}
+
+/* Tests if isSafe tells if a square has a mine */
+TEST(FieldTest, isSafeUnsafetyTest)
+{
+   Field minefield(3);
+
+   minefield.placeMine(1, 1);
+   ASSERT_FALSE(minefield.isSafe(1, 1));
+}
+
+/* Tests if revealAdjacent looks for out of boundsness */
+/*
+TEST(FieldTest, revealAdjacentOutBounds)
+{
+   Field minefield(3);
+
+   ASSERT_NO_FATAL_FAILURE(minefield.revealAdjacent(12, 12));
+}
+*/
+
+/* Tests is revealAdjacent operates correctly for emptyness */
+TEST(FieldTest, revealAdjacentEmptySquare)
+{
+   Field minefield(3);
+
+   minefield.revealAdjacent(1, 1);
+   ASSERT_EQ(EMPTY_SHOWN, minefield.get(1, 1));
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,7 @@
 # BUG LOG
+
+** isSafe does not throw anthing when an out of bounds error occurs
+
+** revealAdjacent segfaults when something out of bounds is passed.
+
+** reavealAdjacent does not make the square passed in empty.
